e9eb2b848742495c90e2d5ca1a726db41ee223a2
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 12:29:54 +0530

    created

---
+++ pro6.java
@@ -0,0 +1,8 @@
+class pro6{
+    public static void main(String[] args){
+        String f1 = new String("apple");
+         String f2 = new String("apple");
+         System.out.println(f1.equals(f2));//string comparison
+         System.out.println(f1==(f2));// reference
+    }
+}
