a94efe9b55ffff2e9d8bbf435f97609dea302b03
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 21:42:54 +0530

    created

--- pro21.java
+++ pro21.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 class pro20{
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        for(int i =1;i<=10;i+2){
-            System.out.println(i);
+        for(int i =1;i<=11;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+            
 
         }
     }
