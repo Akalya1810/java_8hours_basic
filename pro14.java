64b3c3d66db48f42a9d07e40a40936223edaf164
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 16:49:52 +0530

    created success

---
+++ pro14.java
@@ -0,0 +1,18 @@
+class pro14{
+    public static void main(String[] args){
+        boolean kfc = true;
+        boolean chicken = true;
+        boolean pepsi = true;
+        
+        if(kfc){
+            System.out.println("enter the shop");
+        
+        if(chicken){
+            System.out.println("eat");
+            if(pepsi){
+                 System.out.println("drink");
+            }
+        }
+    }
+}
+}
