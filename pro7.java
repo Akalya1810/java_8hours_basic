cc7c69aa85875ac23028412c044382435b70e06a
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 12:52:05 +0530

    created

---
+++ pro7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class pro6{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String rcb = sc.next();
+        if(rcb.equals("win")){
+            System.out.println("cup");
+
+        }
+        else{
+            System.out.println("Lose cup");
+        }
+    }
+}
+
+        
