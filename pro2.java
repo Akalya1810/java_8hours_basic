9a734b88f5199833ab14640e50ec5877e10eaeca
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 10:46:07 +0530

    created

---
+++ pro2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class pro2{
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        String Name = sc.nextLine();
+        int age = sc.nextInt();
+        sc.nextLine();
+        String add = sc.next();
+        System.out.println("Name : " +Name);
+         System.out.println("Age : " +age);
+          System.out.println("Add : " +add);
+
+    }
+}
