be41357b4e2aa5bb163dfcbcfa9515e29e92a9e1
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 10:31:45 +0530

    created successfully

---
+++ pro1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class pro1{
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        String Name = sc.nextLine();
+        int age = sc.nextInt();
+
+    }
+}
