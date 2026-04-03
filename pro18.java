bbfb1f2938a46f7b767584e3fea88f4671a4c116
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 20:16:33 +0530

    created

---
+++ pro18.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class pro13{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n1= sc.nextInt();
+        int n2 = sc.nextInt();
+        int great = (n1>n2)?n1:n2;
+        System.out.println(great);
+        
+    }
+}
