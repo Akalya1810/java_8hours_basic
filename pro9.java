69a83752c3282168b02010910246419a9ad45da3
Akalya1810 <akalyahas1877@gmail.com>
2026-04-03 13:26:17 +0530

    created success

---
+++ pro9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class pro9{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>35){
+            System.out.println("pass");
+        }
+        else{
+             System.out.println("fail");
+
+        }
+    }
+}
