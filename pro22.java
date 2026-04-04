28046ab27ab4969193407673adc8a770a6b8d4a9
Akalya1810 <akalyahas1877@gmail.com>
2026-04-04 10:52:09 +0530

    created

---
+++ pro22.java
@@ -0,0 +1,18 @@
+
+public class pro22 {
+    public static void main(String[] args){
+        int oddCount = 0;
+        for(int i =0;i<=10;i++){
+            if(i%2==0){
+
+            }
+            else{
+                oddCount+=1;
+
+            }
+
+        }
+        System.out.println(oddCount);
+    }
+    
+}
