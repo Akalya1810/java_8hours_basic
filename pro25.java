402f7b53e57b63babba442b72ef476ed8fcf27ed
Akalya1810 <akalyahas1877@gmail.com>
2026-04-04 11:31:48 +0530

    created

---
+++ pro25.java
@@ -0,0 +1,19 @@
+public class pro25 {
+    public static void main(String[] args) {
+        // String[] playlist = new String[3];
+        // playlist[0] = "song 1";
+        // playlist[1] = "song 2";
+        // playlist[2] = "song 3";
+        // System.out.println(playlist[0]);
+        // System.out.println(playlist[1]);
+        // System.out.println(playlist[2]);
+
+                    // [or]
+                    String [] playlist = {"song 1","song 2","song 3"};
+                    System.out.println(playlist[0]);
+                    System.out.println(playlist[1]);
+                    System.out.println(playlist[2]);
+
+    }
+    
+}
