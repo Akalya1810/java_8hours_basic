3994289912ab770722641421b333ea1249982849
Akalya1810 <akalyahas1877@gmail.com>
2026-04-04 13:11:49 +0530

    created

---
+++ pro28.java
@@ -0,0 +1,20 @@
+
+    import java.util.Scanner;
+public class pro28 {
+    public static void main(String[] args){
+        int [] arr = new int[10];
+        Scanner sc = new Scanner(System.in);
+        for(int i =0;i<=9;i++){
+            arr[i] =sc.nextInt();
+            
+
+        }
+       for(int i = 0; i <= 9; i++){
+            System.out.println(arr[i]);
+       }
+    }
+    
+}
+
+    
+
