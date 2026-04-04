db151da615c15ad2f5d24a5559d708dfc5922cd7
Akalya1810 <akalyahas1877@gmail.com>
2026-04-04 12:25:16 +0530

    created

---
+++ pro26.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class pro26 {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        Scanner sc =new Scanner(System.in);
+        array[0]= sc.nextInt();
+        array[1]= sc.nextInt();
+        array[2]= sc.nextInt();
+        array[3]= sc.nextInt();
+        array[4]= sc.nextInt();
+        int arr = array[0]+array[1]+array[2]+array[3]+array[4];
+
+        System.out.println(arr);
+        
+
+
+    }
+    
+}
