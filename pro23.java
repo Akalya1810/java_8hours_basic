6f9f0f719421d1cd6e3781401878a239d5a15759
Akalya1810 <akalyahas1877@gmail.com>
2026-04-04 11:08:38 +0530

    created

---
+++ pro23.java
@@ -0,0 +1,20 @@
+
+public class pro23 {
+    
+
+    public static void main(String[] args){
+        int evenCount = 0;
+        for(int i =0;i<=10;i++){
+            if(i%2==0){
+                evenCount+=1;
+
+            }
+            
+        }
+        System.out.println(evenCount);
+    }
+    
+
+
+    
+}
