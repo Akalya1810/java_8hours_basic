c424b9e56f224fb423766b2bdcce7d4929396423
Akalya1810 <akalyahas1877@gmail.com>
2026-04-04 12:50:37 +0530

    created

---
+++ pro27.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class pro27 {
+    public static void main(String[] args) {
+        int[] marks = new int[5];
+        Scanner sc =new Scanner(System.in);
+        for(int i=0;i<=4;i++){
+        marks[i]=sc.nextInt();
+        }
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+
+       
+    }
+}
