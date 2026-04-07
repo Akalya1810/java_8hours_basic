3788c73130d3fabaf68ad74f6f485f44fcff1615
Akalya1810 <akalyahas1877@gmail.com>
2026-04-07 20:20:03 +0530

    created

---
+++ pro36.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class pro36{   //(hotel)
+    int coffee = 10;
+    int tea = 20;
+
+    public static void main(String[]  args){
+        pro36 server1 = new pro36();
+        System.out.println(server1.coffee);
+        pro36 server2 = new pro36();
+        System.out.println(server2.tea);
+
+
+    }
+
+    
+}
