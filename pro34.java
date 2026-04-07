6066ed283e399c840573729b3e667fa69d9efa1b
Akalya1810 <akalyahas1877@gmail.com>
2026-04-07 19:43:26 +0530

    created

---
+++ pro34.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class pro34 {
+    public static void main(String[] args){
+    
+        Random rand = new Random();
+        int j = 0;
+
+        while(j!=6){
+            j= rand.nextInt(11);
+            System.out.println(j);
+            
+        }
+
+    }
+    
+}
