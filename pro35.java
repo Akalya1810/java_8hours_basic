f30dbe2cc7217654e9e9af96a871fbb23144d223
Akalya1810 <akalyahas1877@gmail.com>
2026-04-07 20:06:34 +0530

    created

---
+++ pro35.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class pro35{
+    public static void main(String[] args){
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("Enter the number");
+             count = sc.nextInt();
+            
+            
+
+
+        }while(count < 10);
+
+
+
+    }
+}
