f570fd70dbc781f8facb308db5623e1d643170a9
Akalya1810 <akalyahas1877@gmail.com>
2026-04-08 20:35:56 +0530

    created

---
+++ pro40.java
@@ -0,0 +1,17 @@
+public class pro40 {
+    void sum(int a ,int b)
+    {
+        System.out.println(a+b);
+
+    }
+    public static void main(String[] args){
+        int a= 10;
+        int b = 20;
+        pro40 obj1 = new pro40();
+        obj1.sum(a, b);
+
+
+
+    }
+    
+}
