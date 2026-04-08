9128a19a29991745a7e867e4afa9c0423f718996
Akalya1810 <akalyahas1877@gmail.com>
2026-04-08 20:23:42 +0530

    created

---
+++ pro39.java
@@ -0,0 +1,22 @@
+public class pro39 {
+    void getsoap(int money)
+    {
+        System.out.println(money);
+        System.out.println("purchased");
+
+    }
+    void getchoco(int cash)
+    {
+        System.out.println(cash);
+        System.out.println("purchased 1");
+    }
+    
+    public static void main(String[] args){
+        pro39 obj1 = new pro39();
+        obj1.getsoap(20);
+        obj1.getchoco(25);
+
+
+    }
+    
+}
