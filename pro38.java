07210a4bb0c6d1f26b85319e31f5b3d7ce7d9ca2
Akalya1810 <akalyahas1877@gmail.com>
2026-04-08 20:05:11 +0530

    created

---
+++ pro38.java
@@ -0,0 +1,15 @@
+public class pro38{
+    int apple_price = 20;
+    int apple_count = 5;
+    void total_money()
+    {
+        System.out.println(apple_price*apple_count);
+
+    }
+    public static void main (String[] args){
+
+    pro38 obj1 = new pro38();
+    obj1.total_money();
+
+
+}}
