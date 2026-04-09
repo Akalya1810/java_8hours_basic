e5d1e0d231550c4c6c00f4221a3ea5359cb99838
Akalya1810 <akalyahas1877@gmail.com>
2026-04-09 22:15:53 +0530

    created

---
+++ pro44.java
@@ -0,0 +1,9 @@
+public class pro44 { // Enhanced for loop (for each loop)
+    public static void main(String[] args){
+        String[]cartoon ={"Bubu: akalya","Dudu: vicky"};
+        for(String order:cartoon){
+            System.out.println(order);
+        }
+    }
+    
+}
