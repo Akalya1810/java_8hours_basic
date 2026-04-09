ecbfcdfbae34f40a10dd23ef06b3e55451a838df
Akalya1810 <akalyahas1877@gmail.com>
2026-04-09 20:04:09 +0530

    created

---
+++ pro41.java
@@ -0,0 +1,13 @@
+public class pro41{
+    String getname(String name)
+    {
+        return name;
+    }
+
+    public static void main(String[] args){
+        pro41 obj1 = new pro41();
+        String n =  obj1.getname("akalya");
+        System.out.println(n);
+
+    }
+}
